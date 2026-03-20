0b0fedb3862a74b44fa2cf741a543dbc50a5914f
rafiqwe <rmlmanik502@gmail.com>
2026-03-20 23:55:31 +0600

    Created: Splash page

--- lib/Screens/Splash/splash_screen.dart
+++ lib/Screens/Splash/splash_screen.dart
@@ -1,10 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/Screens/login/login_screen.dart';
+import 'package:instagram_clone/Widgets/ui_helper.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            UiHelper.CustomImage(imageUrl: 'insta_logo.png'),
+            SizedBox(height: 10),
+            UiHelper.CustomImage(imageUrl: 'logo.png'),
+          ],
+        ),
+      ),
+    );
   }
 }
