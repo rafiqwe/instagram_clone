8bc0b9b0c6c6d0bf542ec4a0c7435f24a3615300
rafiqwe <rmlmanik502@gmail.com>
2026-03-21 00:02:35 +0600

    Created: UI helper widgets

--- lib/Widgets/ui_helper.dart
+++ lib/Widgets/ui_helper.dart
@@ -8,25 +8,27 @@ class UiHelper {
     required TextInputType textInputType,
   }) {
     return Container(
-      height: 44,
-      padding: EdgeInsets.symmetric(horizontal: 10),
+      height: 50,
       width: double.infinity,
       decoration: BoxDecoration(
         color: Color(0XFF121212),
-        border: Border.all(color: Color(0XFFFFFFFF)),
+        border: Border.all(color: Colors.white24),
         borderRadius: BorderRadius.circular(5),
       ),
-      child: TextField(
-        controller: controller,
-        obscureText: toHide,
-        keyboardType: textInputType,
-        decoration: InputDecoration(
-          hintText: text,
-          border: InputBorder.none,
-          hintStyle: TextStyle(
-            fontWeight: FontWeight.normal,
-            fontSize: 14,
-            color: Color(0XFFFFFFFF),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: TextField(
+          controller: controller,
+          obscureText: toHide,
+          keyboardType: textInputType,
+          decoration: InputDecoration(
+            hintText: text,
+            border: InputBorder.none,
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 14,
+              color: Color(0XFFFFFFFF),
+            ),
           ),
         ),
       ),
@@ -35,5 +37,49 @@ class UiHelper {
 
   static CustomImage({required String imageUrl}) {
     return Image.asset("assets/images/$imageUrl");
+  }
+
+  static CustomTextButton({
+    required String text,
+    required VoidCallback callFunction,
+  }) {
+    return TextButton(
+      onPressed: () {
+        callFunction();
+      },
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 14, color: Color(0XFF3797EF)),
+      ),
+    );
+  }
+
+  static CustomButton({
+    required String buttonName,
+    required VoidCallback callBackFunction,
+  }) {
+    return SizedBox(
+      height: 44,
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          callBackFunction();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0XFF3797EF),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        ),
+        child: Center(
+          child: Text(
+            buttonName,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
