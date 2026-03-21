6e998ac7d429075a3cf71a78d5c76d8667ea8a49
rafiqwe <rmlmanik502@gmail.com>
2026-03-22 00:46:29 +0600

    Created: Post Screen

---
+++ lib/Screens/post/post_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PostScreen extends StatelessWidget {
+  const PostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('post page')));
+    ;
+  }
+}
